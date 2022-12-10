7b27d28671daa7f6ae66940d5b8b4255e9317666
Matrix0321 <44547719+Matrix0321@users.noreply.github.com>
2022-12-10 16:58:12 +0800

    Add files via upload

---
+++ HW0/DrawingTriangle.java
@@ -0,0 +1,22 @@
+package yousaywhy;
+
+public class DrawingTriangle {
+    public static void DrawingTriangle(int N){
+
+        int row = 0;
+        int SIZE = N;
+        while (row <= SIZE){
+            int col = 0;
+            while (col < row){
+                System.out.print('*');
+                col = col + 1;
+            }
+            System.out.println();
+            row = row + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        DrawingTriangle(20);
+    }
+}
